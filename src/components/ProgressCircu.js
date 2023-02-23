edfaaf38431536948d35b5fcb9e883de03da0f1c
JordanSissilian <121682392+JordanSissilian@users.noreply.github.com>
2023-02-23 15:38:02 +0100

    fix bug

--- src/components/ProgressCircu.js
+++ src/components/ProgressCircu.js
@@ -1,188 +1,141 @@
-import 'react-circular-progressbar/dist/styles.css';
+import { useState } from 'react';
 import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+
 import size from '../data/QuestionAndAnswer.json';
+import 'react-circular-progressbar/dist/styles.css';
 import logo_stress from '../assets/logo_stress.png';
+
 const xMax = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-
-
-function ProgressCircu({ QuestionAndAnswer, Count }) {
-let progress1 = 0;
-let progress2 = 0;
-let progress3 = 0;
-let progress4 = 0;
-let progress5 = 0;
-let progress6 = 0;
-    // console.log(count);
-
-    // testa = count ? count : 0;
-
-    // console.log("testa", testa);
-
+function ProgressCircu({ QuestionAndAnswer, Count , Progress}) {
     let testa = Count === 8 ? 7 : Count;
-    console.log("testa", testa);
-
     let Answer = localStorage.getItem(QuestionAndAnswer.Formulaire[testa > 0 ? testa - 1: testa].Question);
-    console.log("questionAnswer", QuestionAndAnswer.Formulaire[testa > 0 ? testa - 1: testa].Question)
 
-
-    if (QuestionAndAnswer.Formulaire[testa].Question){
-            switch (QuestionAndAnswer.Formulaire[testa].Question) {
-                case "Sommeil dans les dernières 24h ?"
-                    :progress1 = Math.round(((Answer) / (size.Formulaire[testa].Answer.length - 1)) * 100);
-                    console.log("progress1", Math.round(((Answer) / (size.Formulaire[testa].Answer.length - 1)) * 100))
-                    console.log(Answer + "/ " + (size.Formulaire[testa].Answer.length - 1))
-                    console.log("progress1", progress1)
-                    break;
-                case "Fatigue"
-                    :progress2 = Math.round(((Answer) / (size.Formulaire[testa].Answer.length - 1)) * 100);
-                    console.log(Answer + "/ " + (size.Formulaire[testa].Answer.length - 1))
-                    break;
-                case "Pression ressentie (complexité,experience, enjeu, expertise, relation...)"
-                    :progress3 = Math.round((Answer / 6) * 100);
-                    console.log(Answer + "/ " + 6)
-                    localStorage.setItem("tmp", 0)
-                    break;
-                case "Médicament, Alcool.."
-                    :progress4 = Math.round((Answer / (size.Formulaire[testa].Answer.length - 1)) * 100);
-                    console.log(Answer + "/ " + (size.Formulaire[testa].Answer.length - 1))
-                    break;
-                case "Horaire d'activité"
-                    :progress5 = Math.round((Answer / (size.Formulaire[testa].Answer.length - 1)) * 100);
-                    console.log(Answer + "/ " + (size.Formulaire[testa].Answer.length - 1))
-                    break;
-                case "Etat"
-                    :progress6 = Math.round((Answer / (size.Formulaire[testa].Answer.length - 1)) * 100);
-                    console.log(Answer + "/ " + (size.Formulaire[testa].Answer.length - 1))
-                    break;
-                default:
-                    break;
-            }
-           }
-    console.log("progress1", progress1 ,"progress2", progress2 ,"progress3", progress3 ,"progress4", progress4 ,"progress5", progress5 ,"progress6", progress6)
+    if (QuestionAndAnswer.Formulaire[testa].Question) {
+        switch (QuestionAndAnswer.Formulaire[testa].Question) {
+            case "Sommeil dans les dernières 24h ?" :
+                Progress[0] = ({res: Math.round(((Answer) / (size.Formulaire[testa].Answer.length - 1)) * 100)});
+                break;
+            case "Fatigue" :
+                Progress[1] = ({res: Math.round(((Answer) / (size.Formulaire[testa].Answer.length - 1)) * 100)});
+                break;
+            case "Pression ressentie (complexité,experience, enjeu, expertise, relation...)" :
+                Progress[2] = ({res: Math.round((Answer / 6) * 100)});
+                localStorage.setItem("tmp", 0)
+                break;
+            case "Médicament, Alcool.." :
+                Progress[3] = ({res: Math.round((Answer / (size.Formulaire[testa].Answer.length - 1)) * 100)});
+                break;
+            case "Horaire d'activité" :
+                Progress[4] = ({res: Math.round((Answer / (size.Formulaire[testa].Answer.length - 1)) * 100)});
+                break;
+            case "Etat" :
+                Progress[5] = ({res: Math.round((Answer / (size.Formulaire[testa].Answer.length - 1)) * 100)});
+                break;
+            default:
+                break;
+        }
+    }
+    console.log(Progress);
     return (
         <body>
             <div style={{width: '80%'}}>
                 <div style={styles.contain}>
+
                     <div style={styles.spaceElement}>
-                    <CircularProgressbarWithChildren value={testa > 0 ? 100 : 0} styles={progress1 > 0 && progress1 <= 33 ? styles.green : progress1 > 33 && progress1 <= 66 ? styles.yellow : styles.red}>
-                    <div style={{ fontSize: '100%', paddingBottom: 5 }}>🛌</div>
-                        <div style={{ fontSize: '40%', marginTop: -5 }}>
-                            <strong>{testa > 0 ? 100 : 0}%</strong>  Sommeil
-                            <strong>{progress1}</strong>
-                        </div>
-                    </CircularProgressbarWithChildren>
-                    </div>
-                    <div style={styles.spaceElement}>
-                    <CircularProgressbarWithChildren value={testa >= 2 ? 100 : 0} styles={progress2 > 0 && progress2 <= 33 ? styles.green : progress2 > 33 && progress2 <= 66 ? styles.yellow : styles.red}>
-                    <div style={{ fontSize: '100%', paddingBottom: 5 }}>😴</div>
-                        <div style={{ fontSize: '40%', marginTop: -5 }}>
-                            <strong>{testa >= 2 ? 100 : 0}%</strong> Fatigue
-                        </div>
-                    </CircularProgressbarWithChildren>
+                        <CircularProgressbarWithChildren value={testa > 0 ? 100 : 0} styles={Progress[0].res > 0 && Progress[0].res <= 33 ? styles.green : Progress[0].res > 33 && Progress[0].res <= 66 ? styles.yellow : styles.red}>
+                            <div style={{ fontSize: '100%', paddingBottom: 5 }}>🛌</div>
+                            <div style={{ fontSize: '40%', marginTop: -5 }}>
+                                <strong>{testa > 0 ? 100 : 0}%</strong>  Sommeil
+                            </div>
+                        </CircularProgressbarWithChildren>
                     </div>
 
                     <div style={styles.spaceElement}>
-                    <CircularProgressbarWithChildren value={testa === 3 ? 33 : testa === 4 ? 66 : testa >= 5 ? 100 : 0} styles={progress3 > 0 && progress3 <= 33 ? styles.green : progress3 > 33 && progress3 <= 66 ? styles.yellow : styles.red}>
-                    <img style={{ width: '40%', marginTop: -5}} src={logo_stress} alt="doge" />
-                        <div style={{ fontSize: '40%', marginTop: -5 , marginLeft: 10}}>
-                            <strong>{testa === 3 ? 33 : testa === 4 ? 66 : testa >= 5 ? 100 : 0}%</strong> Etat psycologique
-                        </div>
-                    </CircularProgressbarWithChildren>
+                        <CircularProgressbarWithChildren value={testa >= 2 ? 100 : 0} styles={Progress[1].res > 0 && Progress[1].res <= 33 ? styles.green : Progress[1].res > 33 && Progress[1].res <= 66 ? styles.yellow : styles.red}>
+                            <div style={{ fontSize: '100%', paddingBottom: 5 }}>😴</div>
+                            <div style={{ fontSize: '40%', marginTop: -5 }}>
+                                <strong>{testa >= 2 ? 100 : 0}%</strong> Fatigue
+                            </div>
+                        </CircularProgressbarWithChildren>
+                    </div>
+
+                    <div style={styles.spaceElement}>
+                        <CircularProgressbarWithChildren value={testa === 3 ? 33 : testa === 4 ? 66 : testa >= 5 ? 100 : 0} styles={Progress[2].res > 0 && Progress[2].res <= 33 ? styles.green : Progress[2].res > 33 && Progress[2].res <= 66 ? styles.yellow : styles.red}>
+                            <img style={{ width: '40%', marginTop: -5}} src={logo_stress} alt="doge" />
+                            <div style={{ fontSize: '40%', marginTop: -5 , marginLeft: 10}}>
+                                <strong>{testa === 3 ? 33 : testa === 4 ? 66 : testa >= 5 ? 100 : 0}%</strong> Etat psycologique
+                            </div>
+                        </CircularProgressbarWithChildren>
                     </div>
                     
                     <div style={styles.spaceElement}>
-                    <CircularProgressbarWithChildren value={testa >= 6 ? 100 : 0} styles={progress4 > 0 && progress4 <= 33 ? styles.green : progress4 > 33 && progress4 <= 66 ? styles.yellow : styles.red}>
-                    <div style={{ fontSize: '100%', paddingBottom: 5 }}>💊</div>
-                        <div style={{ fontSize: '40%', marginTop: -5 , marginLeft: 10}}>
-                            <strong>{testa >= 6 ? 100 : 0}%</strong> Medicament, Alcool..
-                        </div>
-                    </CircularProgressbarWithChildren>
+                        <CircularProgressbarWithChildren value={testa >= 6 ? 100 : 0} styles={Progress[3].res > 0 && Progress[3].res <= 33 ? styles.green : Progress[3].res > 33 && Progress[3].res <= 66 ? styles.yellow : styles.red}>
+                            <div style={{ fontSize: '100%', paddingBottom: 5 }}>💊</div>
+                            <div style={{ fontSize: '40%', marginTop: -5 , marginLeft: 10}}>
+                                <strong>{testa >= 6 ? 100 : 0}%</strong> Medicament, Alcool..
+                            </div>
+                        </CircularProgressbarWithChildren>
                     </div>
-                    <div style={styles.spaceElement}>
 
-                    <CircularProgressbarWithChildren value={testa >= 7 ? 100 : 0} styles={progress5 > 0 && progress5 <= 33 ? styles.green : progress5 > 33 && progress5 <= 66 ? styles.yellow : styles.red}>
-                    <div style={{ fontSize: '100%', paddingBottom: 5 }}>🕦🧑‍💻 </div>
-                        <div style={{ fontSize: '40%', marginTop: -5 , marginLeft: 10}}>
-                            <strong>{testa >= 7 ? 100 : 0}%</strong> Horaire d'activité
-                        </div>
-                    </CircularProgressbarWithChildren>
-                    </div>
                     <div style={styles.spaceElement}>
-                        
-                    <CircularProgressbarWithChildren value={testa >= 8 ? 100 : 0} styles={progress6 > 0 && progress6 <= 33 ? styles.green : progress6 > 33 && progress6 <= 66 ? styles.yellow : styles.red}>
-                    <div style={{ fontSize: '100%', paddingBottom: 5 }}>🤒</div>
-                        <div style={{ fontSize: '40%', marginTop: -5 }}>
-                            <strong>{testa >= 8 ? 100 : 0}%</strong> ETAT
-                        </div>
-                    </CircularProgressbarWithChildren>
+                        <CircularProgressbarWithChildren value={testa >= 7 ? 100 : 0} styles={Progress[4].res > 0 && Progress[4].res <= 33 ? styles.green : Progress[4].res > 33 && Progress[4].res <= 66 ? styles.yellow : styles.red}>
+                            <div style={{ fontSize: '100%', paddingBottom: 5 }}>🕦🧑‍💻 </div>
+                            <div style={{ fontSize: '40%', marginTop: -5 , marginLeft: 10}}>
+                                <strong>{testa >= 7 ? 100 : 0}%</strong> Horaire d'activité
+                            </div>
+                        </CircularProgressbarWithChildren>
                     </div>
+
+                    <div style={styles.spaceElement}>
+                        <CircularProgressbarWithChildren value={testa >= 8 ? 100 : 0} styles={Progress[5].res > 0 && Progress[5].res <= 33 ? styles.green : Progress[5].res > 33 && Progress[5].res <= 66 ? styles.yellow : styles.red}>
+                            <div style={{ fontSize: '100%', paddingBottom: 5 }}>🤒</div>
+                            <div style={{ fontSize: '40%', marginTop: -5 }}>
+                                <strong>{testa >= 8 ? 100 : 0}%</strong> ETAT
+                            </div>
+                        </CircularProgressbarWithChildren>
+                    </div>
+    
                 </div>
             </div>
-            
         </body>
     );
-  }
-  export default ProgressCircu;
+}
+export default ProgressCircu;
 
-  const styles = {
-	red: {
-		trail: {
-            stroke: '#d6d6d6',
-        },
+const styles = {
+    red: {
+		trail: { stroke: '#d6d6d6', },
         text: {
             fill: '#1e90ff',
             fontSize: '16px',
         },
-        background: {
-            fill: '#3e98c7',
-        },
-		path: {
-			stroke: 'red',
-		}
+        background: { fill: '#3e98c7', },
+		path: { stroke: 'red', }
 	},
 	yellow: {
-		trail: {
-            stroke: '#d6d6d6',
-        },
+		trail: { stroke: '#d6d6d6', },
         text: {
             fill: '#1e90ff',
             fontSize: '16px',
         },
-        background: {
-            fill: '#3e98c7',
-        },
-		path: {
-			stroke: 'yellow',
-			
-		}
+        background: { fill: '#3e98c7', },
+		path: { stroke: 'yellow', }
 	},
 	green: {
-		trail: {
-            stroke: '#d6d6d6',
-        },
+		trail: { stroke: '#d6d6d6', },
         text: {
             fill: '#1e90ff',
             fontSize: '16px',
         },
-        background: {
-            fill: '#3e98c7',
-        },
-		path: {
-			stroke: 'green',
-		}
+        background: { fill: '#3e98c7', },
+		path: { stroke: 'green', }
 	},
-
     contain: {
         flex: 1,
-        //tialle
         width: '124%',
-       // marginLeft: xMax < 600 ? '20%' : 0,
-        //metre tout les items en ligne
         display: 'flex',
-        //metre un espace entre les items
         justifyContent: 'space-between',
-       // flexDirection: xMax < 600 ? 'column' : 'row',
     },
     spaceElement: {
         marginLeft: xMax < 600 ? 0 : 20,
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,121 +1,108 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router';
+
 import QuestionAndAnswer from '../data/QuestionAndAnswer.json';
 import ButtonAnswer from './ButtonAnswer';
 import ProgressCircu from './ProgressCircu';
-import RatingMeter from './RatingMeter/rating_meter';
 import ProgressBar from './progress-bar/progress-bar';
-import { Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router';
 
 function Question() {
-  //increment the count to display the next question
-  const [count, setCount] = useState(0);    
+  const [count, setCount] = useState(0);
+  const [progress, setProgress] = useState([{res: 0}, {res: 0}, {res: 0}, {res: 0}, {res: 0}, {res: 0}]); 
   const navigate = useNavigate();
-  function handleSubmit(e) {
-    e.preventDefault();
 
-    if (count < QuestionAndAnswer.Formulaire.length){
-      setCount(count + 1);
-      localStorage.setItem("count",count == null ? 0 : count)
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (count < QuestionAndAnswer.Formulaire.length) {
+            setCount(count + 1);
+            localStorage.setItem("count",count == null ? 0 : count);
+        }
     }
-    
-  }
-  function showresult(){
-    navigate('/FILGOODS/result');
-  }
-  function handleBack(e){
-    e.preventDefault();
-   if (count > 0){
-      QuestionAndAnswer.Formulaire[count].Question = null
-      setCount(count - 1);
-      localStorage.setItem("count", count)
-      console.log("aaaa",QuestionAndAnswer.Formulaire[count].Question)
-    }
-  }
-  if (count < QuestionAndAnswer.Formulaire.length){
-    console.log("count",count, "taille", QuestionAndAnswer.Formulaire.length);
-    return (
-      <div className="App-header" >
-        <text>{QuestionAndAnswer.Formulaire[count].Question}</text>
-        <ProgressBar key={1}  completed={(count / QuestionAndAnswer.Formulaire.length) * 100} />
-        <body className='App-body' style={styles.bodyQuestion}>     
-          <div>
-            <br></br>
-            <div style={styles.divcenter} >
-              <form onSubmit={handleSubmit} >
-              <ButtonAnswer QuestionAndAnswer={QuestionAndAnswer.Formulaire[count]}></ButtonAnswer>
-              {count === 5 ? <button style={styles.button} >Je ne souhaite pas répondre </button> : false}
-              <button style={styles.button} onClick={handleBack} >Retour</button>
 
-              </form>
-          </div>
-          
-          </div>
-          
-          <br/>
-          <br/>
-          <br/>
-          <ProgressCircu QuestionAndAnswer={QuestionAndAnswer} Count={count} ></ProgressCircu>      
-        </body>
-      </div>
-    );
-  }
-  else
-  {
-    return(
-      <div className="App-header" >
-      <div style={styles.ratingmeter}>
-        <button style={styles.buttonconfirm} onClick={showresult}>Voir resultat </button>
-        <button style={styles.button} onClick={handleBack} >Retour</button>
-        </div>
-        <body className='App-body' style={styles.bodyQuestion}> 
-          <ProgressCircu QuestionAndAnswer={QuestionAndAnswer} Count={count}  ></ProgressCircu>      
-        </body>
-      </div>
-   )
-  }
+    function handleBack(e) {
+        e.preventDefault();
+        if (count > 0){
+            QuestionAndAnswer.Formulaire[count].Question = null
+            setCount(count - 1);
+            localStorage.setItem("count", count);
+        }
+    }
+
+    if (count < QuestionAndAnswer.Formulaire.length) {
+        return (
+            <div className="App-header" >
+                <text>{QuestionAndAnswer.Formulaire[count].Question}</text>
+                <ProgressBar key={1}  completed={(count / QuestionAndAnswer.Formulaire.length) * 100} />
+                <body className='App-body' style={styles.bodyQuestion}>    
+                    <div>
+                        <br></br>
+                        <div style={styles.divcenter} >
+                            <form onSubmit={handleSubmit} >
+                                <ButtonAnswer QuestionAndAnswer={QuestionAndAnswer.Formulaire[count]} />
+                                {count === 5 ? <button style={styles.button} >Je ne souhaite pas répondre </button> : false}
+                                <button style={styles.button} onClick={handleBack} >Retour</button>
+                            </form>
+                        </div>
+                    </div>
+                    <br/><br/><br/>
+                    <ProgressCircu QuestionAndAnswer={QuestionAndAnswer} Count={count} Progress={progress} />
+                </body>
+            </div>
+        );
+    } else {
+        return (
+            <div className="App-header" >
+                <div style={styles.ratingmeter}>
+                    <button style={styles.buttonconfirm} onClick={() => navigate('/FILGOODS/result')} >Voir resultat</button>
+                    <button style={styles.button} onClick={(handleBack)} >Retour</button>
+                </div>
+                <body className='App-body' style={styles.bodyQuestion} > 
+                    <ProgressCircu QuestionAndAnswer={QuestionAndAnswer} Count={count} Progress={progress} />   
+                </body>
+            </div>
+        );
+    }
 }
 
 export default Question;
+
 const styles = {
-  
-  ratingmeter: {
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-  },
-   bodyQuestion: {
+    ratingmeter: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+    },
+    bodyQuestion: {
         shadowColor: "white",
         borderRadius: '10%',
         width: '100%',
         height: '-100%',
         padding: '10%,'
-  },
-  buttonconfirm: {
-    backgroundColor: '#1e90ff',
-    color: 'white',
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    fontSize: '16px',
-    fontWeight: 'bold',
-    margin: '10px'
-},
-  button: {
-      backgroundColor: '#1e90ff',
-      color: 'white',
-      padding: '10px 20px',
-      border: 'none',
-      borderRadius: '5px',
-      cursor: 'pointer',
-      fontSize: '16px',
-      fontWeight: 'bold',
-      margin: '10px'
-  },
-  divcenter: {
-    display: 'flex',  justifyContent:'center', alignItems:'center'
-  }
-
+    },
+    buttonconfirm: {
+        backgroundColor: '#1e90ff',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        margin: '10px'
+    },
+    button: {
+        backgroundColor: '#1e90ff',
+        color: 'white',
+        padding: '10px 20px',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        fontSize: '16px',
+        fontWeight: 'bold',
+        margin: '10px'
+    },
+    divcenter: {
+        display: 'flex',  justifyContent:'center', alignItems:'center'
+    }
 }
